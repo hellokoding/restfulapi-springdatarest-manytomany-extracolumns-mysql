84c2bb8bc3922b8ef6b6a203c16e5c504de34dbb
Giau Ngo <giaunv@users.noreply.github.com>
2017-08-13 15:16:11 +0700

    First commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# RESTful API Example with Spring Boot, Spring Data REST Many To Many Extra Columns Relationship and MySQL
+
+## Guide
+https://hellokoding.com/restful-api-example-with-spring-boot-spring-data-rest-and-mysql/
+
+## Prerequisites
+- JDK 1.7+
+- Maven 3+
+- MySQL 5.6+
+
+## Stack
+- Spring Boot
+- Spring Data JPA
+- Spring Data REST
+- MySQL
+
+## Run
+`mvn clean spring-boot:run`
---
+++ src/main/java/com/hellokoding/restfulapi/model/BookPublisher.java
@@ -0,0 +1,53 @@
+package com.hellokoding.restfulapi.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Table(name = "book_publisher")
+public class BookPublisher implements Serializable{
+    private int id;
+    private Book book;
+    private Publisher publisher;
+    private Date publishedDate;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "book_id")
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "publisher_id")
+    public Publisher getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(Publisher publisher) {
+        this.publisher = publisher;
+    }
+
+    @Column(name = "published_date")
+    public Date getPublishedDate() {
+        return publishedDate;
+    }
+
+    public void setPublishedDate(Date publishedDate) {
+        this.publishedDate = publishedDate;
+    }
+}
